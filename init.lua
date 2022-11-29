f0c4056f2d99643a70a1a238c91e313757b2fa25
flux <25628292+fluxionary@users.noreply.github.com>
2022-11-29 11:55:56 -0800

    fmod and normalization

--- init.lua
+++ init.lua
@@ -1,32 +1,4 @@
-local f = string.format
-
-local modname = minetest.get_current_modname()
-local modpath = minetest.get_modpath(modname)
-local S = minetest.get_translator(modname)
-
-itemplate = {
-	author = "flux",
-	license = "AGPL_v3",
-	version = os.time({ year = 2022, month = 10, day = 28 }),
-	fork = "flux",
-
-	modname = modname,
-	modpath = modpath,
-	S = S,
-
-	has = {
-		default = minetest.get_modpath("default"),
-		node_entity_queue = minetest.get_modpath("node_entity_queue"),
-	},
-
-	log = function(level, messagefmt, ...)
-		return minetest.log(level, f("[%s] %s", modname, f(messagefmt, ...)))
-	end,
-
-	dofile = function(...)
-		return dofile(table.concat({ modpath, ... }, DIR_DELIM) .. ".lua")
-	end,
-}
+itemplate = fmod.create()
 
 itemplate.dofile("settings")
 itemplate.dofile("resources")
--- settings.lua
+++ settings.lua
@@ -1,5 +1,6 @@
 local s = minetest.settings
 
+-- we're stealing a setting from itemframes, so we can't rely on fmod
 itemplate.settings = {
 	return_item = s:get_bool("itemframes.return_item", true),
 }
